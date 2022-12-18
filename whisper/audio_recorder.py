e249f48b584af621f0717a22964fd924bdf9330c
Shubhushan Kattel <shubhushankattel@gmail.com>
2022-12-18 12:55:10 +0100

    Created an audio recorder to use for whisper ai

---
+++ whisper/audio_recorder.py
@@ -0,0 +1,88 @@
+import os
+import threading
+import pyaudio
+import wave
+import time
+
+frames = []
+
+sample_format = pyaudio.paInt32
+fs = 44100
+channels = 2
+CHUNK=1024
+
+kill_threads = False
+
+def record_audio():
+    p = pyaudio.PyAudio()
+    stream = p.open(format=sample_format,
+        channels=channels,
+        rate=fs,
+        frames_per_buffer=CHUNK,
+        input=True)
+    while True:
+        if kill_threads:
+            return
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+
+
+def save_audio(filename="output.wav"):
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(pyaudio.PyAudio().get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b"".join(frames))
+    wf.close
+
+def timer_thread():
+    x = 0
+    while True:
+        if kill_threads:
+            return
+        time.sleep(0.1)
+        x += 0.1
+        print(f"{x} seconds of sound recorded")
+
+class RecordingThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        record_audio()
+
+class TimingThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        timer_thread()
+
+
+def run_threads(filename=None):
+    #recording_thread = threading.Thread(target=record_audio)
+    #timing_thread = threading.Thread(target=timer_thread)
+    recording_thread = RecordingThread()
+    timing_thread = TimingThread()
+    recording_thread.start()
+    timing_thread.start()
+
+    user_input = input("Press any key to stop recording audio\n")
+    global kill_threads
+
+    if user_input:
+        print(f"Hi this is user input: {user_input}")
+        kill_threads = True
+    
+    recording_thread.join()
+    timing_thread.join()
+
+    if filename is not None:
+        save_audio(filename=filename)
+    else:
+        save_audio()
+
+
+if __name__ == "__main__":
+    run_threads()
--- whisper/main.py
+++ whisper/main.py
@@ -1,47 +1,17 @@
 import whisper
-
-import pyaudio
-import wave
 import sys
+from audio_recorder import run_threads
 
-CHUNK = 1024
-sample_format = pyaudio.paInt32
-fs = 44100
-seconds = 10
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+
+print(torch.cuda.is_available())
+
 filename = "output.wav"
-channels=2
 
-p = pyaudio.PyAudio()
+run_threads()
 
-stream = p.open(format=sample_format,
-            channels=channels,
-            rate=fs,
-            frames_per_buffer=CHUNK,
-            input=True)
-
-frames = []
-
-record_audio=input("Do you want to record audio(y/n): ")
-
-if record_audio == 'y':
-    print("Started recording audio")
-    for i in range(0, int(fs/CHUNK*seconds)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-    print("Stopped recording audio")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    # save file
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
-    wf.writeframes(b"".join(frames))
-    wf.close()
-
-model = whisper.load_model('medium').to('cuda')
+model = whisper.load_model('base').to('cuda')
 
 result = model.transcribe(filename)
 print(result['text'], result['language'])
@@ -55,6 +25,15 @@ options = whisper.DecodingOptions(task='translate')
 
 result_ = whisper.decode(model, mel, options)
 print(result_.text)
+
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+image = pipe(result_.text).images[0]
+image.save("trial.png")
 
 # import whisper
 # model = whisper.load_model('base').to('cuda')
